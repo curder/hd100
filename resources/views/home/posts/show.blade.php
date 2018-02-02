6f8b7e96c2cd715522af62810ed341ff84b5f39e
curder <curder@foxmail.com>
2018-02-02 15:13:57 +0800

    默认的文章发布者标签写死在模板里

--- resources/views/home/posts/show.blade.php
+++ resources/views/home/posts/show.blade.php
@@ -1,14 +1,18 @@
 @extends('home.layouts.master')
-@section('page_title',$post->title)
+@section('page_title',$post->seo_title ?? $post->title)
+@section('page_keywords',$post->seo_keywords)
+@section('page_description',$post->seo_description)
+
 @section('banner')
-    <div class="banner-inner" style="background-image: url({{ $post->category->cover ?? 'images/services.png' }});"></div>
+    <div class="banner-inner"
+         style="background-image: url({{ $post->category->cover ?? 'images/services.png' }});"></div>
 @stop
 @section('content')
     <div id="bd">
         <div class="wp">
             <div class="box-news-info cont-inner">
                 <h1 class="tit">{{ $post->title }}</h1>
-                <p class="date-1">{{ $post->author ?? config('default_news_author') }}</p>
+                <p class="date-1">作者：{{ $post->author ?? config('default_news_author') }}</p>
                 <div class="info">
                     {!! $post->body !!}
                 </div>
