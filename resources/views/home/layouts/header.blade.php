2ec91de1f16c67cf1d840c536dbfc6d4658ce963
curder <curder@foxmail.com>
2018-02-11 14:32:04 +0800

    logo的链接地址改为首页

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <div style="width:100%;position:fixed;top:0;left:0;height:10px;z-index:999;" id="toptop"></div>
 <div id="hd">
     <div class="wp">
-        <a href="" class="logo">
+        <a href="/" class="logo">
             <img src="{{ config('logo') }}" alt="{{ config('domain_title') }}">
         </a>
         <div class="tel">
