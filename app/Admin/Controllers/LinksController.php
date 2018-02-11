4b2f5e0cc557d454c17871bacd33dde8c282221d
curder <curder@foxmail.com>
2018-02-11 13:39:19 +0800

    链接提示信息

--- app/Admin/Controllers/LinksController.php
+++ app/Admin/Controllers/LinksController.php
@@ -12,112 +12,108 @@ use Encore\Admin\Controllers\ModelForm;
 use Encore\Admin\Tree;
 use Illuminate\Support\Facades\Lang;
 
-class LinksController extends Controller
-{
-    use ModelForm;
+class LinksController extends Controller {
+	use ModelForm;
 
-    /**
-     * Index interface.
-     *
-     * @return Content
-     */
-    public function index()
-    {
-        return Admin::content(function (Content $content) {
+	/**
+	 * Index interface.
+	 *
+	 * @return Content
+	 */
+	public function index() {
+		return Admin::content( function ( Content $content ) {
 
-            $content->header('文字连接管理');
-            $content->description('管理网站的所有文字连接，比如导航、友情链接或者底部链接');
+			$content->header( '文字连接管理' );
+			$content->description( '管理网站的所有文字连接，比如导航、友情链接或者底部链接' );
 
-            $content->body($this->tree());
-        });
-    }
+			$content->body( $this->tree() );
+		} );
+	}
 
-    /**
-     * Edit interface.
-     *
-     * @param $id
-     * @return Content
-     */
-    public function edit($id)
-    {
-        return Admin::content(function (Content $content) use ($id) {
+	/**
+	 * Edit interface.
+	 *
+	 * @param $id
+	 *
+	 * @return Content
+	 */
+	public function edit( $id ) {
+		return Admin::content( function ( Content $content ) use ( $id ) {
 
-            $content->header('编辑文字链');
+			$content->header( '编辑文字链' );
 
-            $content->body($this->form()->edit($id));
-        });
-    }
+			$content->body( $this->form()->edit( $id ) );
+		} );
+	}
 
-    /**
-     * Create interface.
-     *
-     * @return Content
-     */
-    public function create()
-    {
-        return Admin::content(function (Content $content) {
+	/**
+	 * Create interface.
+	 *
+	 * @return Content
+	 */
+	public function create() {
+		return Admin::content( function ( Content $content ) {
 
-            $content->header('新增文字接');
+			$content->header( '新增文字接' );
 
-            $content->body($this->form());
-        });
-    }
+			$content->body( $this->form() );
+		} );
+	}
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        return Admin::grid(Link::class, function (Grid $grid) {
+	/**
+	 * Make a grid builder.
+	 *
+	 * @return Grid
+	 */
+	protected function grid() {
+		return Admin::grid( Link::class, function ( Grid $grid ) {
 
-            $grid->id('ID')->sortable();
+			$grid->id( 'ID' )->sortable();
 
-            $grid->created_at();
-            $grid->updated_at();
-        });
-    }
+			$grid->created_at();
+			$grid->updated_at();
+		} );
+	}
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        return Link::form(function (Form $form) {
+	/**
+	 * Make a form builder.
+	 *
+	 * @return Form
+	 */
+	protected function form() {
+		return Link::form( function ( Form $form ) {
 
-            $form->display('id', 'ID');
+			$form->display( 'id', 'ID' );
 
-            $form->select('parent_id','上级文字链')->options(Link::selectOptions());
+			$form->select( 'parent_id', '上级文字链' )->options( Link::selectOptions() );
 
-            $form->text('title','标题')->rules('required');
-            $form->text('url','文章链接地址');
-            $form->textarea('description','描述');
-            $form->image('logo','文字链关联图片')->help('如果文字链存在图片请于此上传，否则为空');
+			$form->text( 'title', '标题' )->rules( 'required' );
+			$form->text( 'url', '文章或者页面地址' )
+			     ->help( '为了方便后期数据的迁移，如果是站内地址，请填写相对地址' );
+			$form->textarea( 'description', '描述' );
+			$form->image( 'logo', '文字链关联图片' )
+			     ->help( '如果文字链存在图片请于此上传，否则为空' );
 
-            $form->display('created_at', Lang::get('admin.created_at'));
-            $form->display('updated_at', Lang::get('admin.updated_at'));
-        });
-    }
+			$form->display( 'created_at', Lang::get( 'admin.created_at' ) );
+			$form->display( 'updated_at', Lang::get( 'admin.updated_at' ) );
+		} );
+	}
 
-    /**
-     * Make a grid builder.
-     */
-    protected function tree()
-    {
-        return Link::tree(function (Tree $tree) {
-            $tree->branch(function ($branch) {
+	/**
+	 * Make a grid builder.
+	 */
+	protected function tree() {
+		return Link::tree( function ( Tree $tree ) {
+			$tree->branch( function ( $branch ) {
 
-                $src = config('admin.upload.host') . '/' . $branch['logo'] ;
+				$src = config( 'admin.upload.host' ) . '/' . $branch['logo'];
 
-                $logo = is_file($src) ? "<img src='$src' style='max-width:30px;max-height:30px' class='img'/>" : '';
+				$logo = is_file( $src ) ? "<img src='$src' style='max-width:30px;max-height:30px' class='img'/>" : '';
 
-                return "{$branch['id']} - {$branch['title']} $logo";
+				return "{$branch['id']} - {$branch['title']} $logo";
 
-            });
+			} );
 
-        });
-    }
+		} );
+	}
 }
