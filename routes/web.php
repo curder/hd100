913e30c76ddc05490d0d920542aa6a00751e96ab
curder <curder@foxmail.com>
2018-03-10 17:55:04 +0800

    添加网站页面'.html'后缀

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,12 @@ Route::group( [
 ], function ( Router $router ) {
 
 	$router->get( '/', 'HomeController@index' )->name( 'index' );
-	$router->get( 'channels-{category}', 'PostsController@index' )->name( 'posts.index' );
-	$router->get( 'news-{post}', 'PostsController@show' )->name( 'posts.show' );
-	$router->get( 'experts', 'ExpertsController@index' )->name( 'experts.index' );
-	$router->get( 'experts-{expert}', 'ExpertsController@show' )->name( 'experts.show' );
+	$router->get( 'channels-{category}.html', 'PostsController@index' )->name( 'posts.index' );
+	$router->get( 'news-{post}.html', 'PostsController@show' )->name( 'posts.show' );
+	$router->get( 'experts.html', 'ExpertsController@index' )->name( 'experts.index' );
+	$router->get( 'experts-{expert}.html', 'ExpertsController@show' )->name( 'experts.show' );
 
-	$router->get( 'pages-{page}', 'PagesController@show' )->name( 'pages.show' ); // 单页面管理
-	$router->get( 'concat-{city?}', 'PagesController@concat' )->name( 'pages.concat' );
+	$router->get( 'pages-{page}.html', 'PagesController@show' )->name( 'pages.show' ); // 单页面管理
+	$router->get( 'concat-{city?}.html', 'PagesController@concat' )->name( 'pages.concat' );
 
 } );
